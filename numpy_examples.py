2110a989126c1ce44bb3b73e43e87f65875ced3b
GigaTortilla <martinappel01@gmail.com>
2024-05-30 08:23:04 +0200

    added numpy example

---
+++ numpy_examples.py
@@ -0,0 +1,12 @@
+import numpy as np
+from example_logs import init_log_dbg
+
+
+def main(argv=None) -> int:
+    init_log_dbg()
+    pass
+
+
+# Example code to showcase logging functionality
+if __name__ == '__main__':
+    raise SystemExit(main())
