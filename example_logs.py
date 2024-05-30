3adca52bf89dafbef2e23dd8d1486a3349784dbb
GigaTortilla <martinappel01@gmail.com>
2024-05-30 13:13:24 +0200

    fixed logging timestamps

--- example_logs.py
+++ example_logs.py
@@ -7,7 +7,8 @@ def init_log(logname: str = "example", \
     start_time = time.localtime()
     logging.basicConfig(filename="logs/" + \
                         "-".join([str(i) for i in start_time[0:3]]) + "_" + \
-                        "-".join([str(i) for i in start_time[3:5]]) + "_" + \
+                        str.format("{:02}-{:02}", start_time.tm_hour, \
+                                   start_time.tm_min) + "_" + \
                         logname + ".log", filemode="w", level=loglevel, \
                         format="%(asctime)s - %(levelname)s::%(module)s::" + \
                         "%(funcName)s at line %(lineno)d - %(message)s")
