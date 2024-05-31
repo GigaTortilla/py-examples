5afbb4b431998f9a38e5231533e4fac515558b8b
GigaTortilla <martinappel01@gmail.com>
2024-05-31 07:05:17 +0200

    completed first example

--- bokeh_examples.py
+++ bokeh_examples.py
@@ -1,5 +1,6 @@
 import numpy as np
-from bokeh.plotting import figure, output_file, show
+from bokeh.plotting import figure, show
+from bokeh.io import output_file
 from bokeh.sampledata.iris import flowers
 
 
@@ -7,5 +8,10 @@ def main(argv=None) -> int:
     # Beispiel-Daten und Ausgabedatei erzeugen
     x = [1, 2, 3, 4, 5]
     y = [5, 6, 1, 3, 4]
-    output_file("line_example.html")
+    output_file("line_example.html", title="Bokeh Examples")
+
+    plot = figure(title="Line Plot", x_axis_label="x axis", \
+                  y_axis_label="y axis")
+    plot.line(x, y, legend="line example", line_width=2)
+    show(plot)
     return 0
