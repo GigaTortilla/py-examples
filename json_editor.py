d7ed810adbb0df7b9746c6d23ed8c931c1351c18
GigaTortilla <martinappel01@gmail.com>
2024-06-13 08:54:59 +0200

    json handling example

---
+++ json_editor.py
@@ -0,0 +1,27 @@
+import json
+
+
+def remove_conditions(file_path: str) -> None:
+    
+    with open(file_path, 'r+') as f:
+        templates = json.load(f)
+        if 'mappings' not in templates:
+            return
+        for i in range(len(templates['mappings'])):
+            if 'conditions' in templates['mappings'][i]:
+                templates['mappings'][i].pop('conditions')
+    
+    with open(file_path, "w") as nf:
+        nf.write(json.dumps(templates, 
+                            sort_keys=False, 
+                            indent=2, 
+                            separators=(',', ': ')))
+
+
+def main(argv=None) -> int:
+    remove_conditions('route_templates.json')
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
